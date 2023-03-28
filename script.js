7b97639c982afdc59f7569d6cee69be036bf69b9
Leocadio <dosleocadio@gmail.com>
2023-03-27 22:21:28 -0300

    PI c MQ

---
+++ script.js
@@ -0,0 +1,41 @@
+var Tino=document.getElementById("Tino");
+
+function txtIno(){
+    var style=Tino.style.display;
+        if(style=='block')
+        	{
+                Tino.style.display='none';
+        	}
+    	else{
+            Tino.style.display='block';
+        	}    			
+    }
+    var Tino=document.getElementById("Tino");
+
+    var Ttrans=document.getElementById("Ttrans");
+
+    function txtTrans(){
+           var style=Ttrans.style.display;
+               if(style=='block')
+                   {
+                       Ttrans.style.display='none';
+                   }
+               else{
+                   Ttrans.style.display='block';
+                   }    			
+           }
+           var Ttrans=document.getElementById("Ttrans");
+
+           var Tres=document.getElementById("Tres");
+
+           function txtRes(){
+                  var style=Tres.style.display;
+                      if(style=='block')
+                          {
+                            Tres.style.display='none';
+                          }
+                      else{
+                        Tres.style.display='block';
+                          }    			
+                  }
+                  var Tres=document.getElementById("Tres");
